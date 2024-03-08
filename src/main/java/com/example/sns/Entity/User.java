8dc6517e22e222417565632eb7f1002c2d0a20a9
iamseung <seesungseok@naver.com>
2024-03-08 23:57:03 +0900

    User Entity 수정
    - 제목과 같습니다

--- src/main/java/com/example/sns/Entity/User.java
+++ src/main/java/com/example/sns/Entity/User.java
@@ -1,9 +1,22 @@
 package com.example.sns.Entity;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
-//@Entity
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Getter
+@Setter
 public class User {
+    @Id
+    private Integer id;
+
+    @Column(name = "user_name")
     private String userName;
     private String password;
 }
